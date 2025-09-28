b1d36a75ecc4cc41f2fe9427ddf1b3ec0f5a7254
Ahmed Mohned <amohned346@gmail.com>
2025-09-28 19:33:09 +0300

    auth: create github login

--- components/forms/SocialAuthForm.tsx
+++ components/forms/SocialAuthForm.tsx
@@ -1,14 +1,26 @@
+"use client";
 import React from "react";
 import Image from "next/image";
 import { Button } from "../ui/button";
 import githubLogo from "@/public/icons/github.svg";
+import ROUTES from "@/constants/routes";
 import googleLogo from "@/public/icons/google.svg";
+import { signIn } from "next-auth/react";
 const SocialAuthForm = () => {
   const buttonClass =
     "background-dark400_light900 body-medium text-dark200_light800 min-h-12 flex-1  px-4 py-3";
+  const handleSignIn = async (provider: "github" | "google") => {
+    // Implement sign-in logic here, e.g., redirect to OAuth provider
+    try {
+      await signIn(provider, {
+        callBackUrl: ROUTES.HOME,
+        redirect: true,
+      });
+    } catch (err) {}
+  };
   return (
     <div className="mt-10 flex flex-wrap gap-2.5">
-      <Button className={buttonClass}>
+      <Button className={buttonClass} onClick={() => handleSignIn("github")}>
         <Image
           src={githubLogo}
           alt="github logo"
@@ -18,7 +30,7 @@ const SocialAuthForm = () => {
         />
         <span>Log in with Github</span>
       </Button>
-      <Button className={buttonClass}>
+      <Button className={buttonClass} onClick={() => handleSignIn("google")}>
         <Image
           src={googleLogo}
           alt="github logo"
